00c8307595b51d0affae6e76fa041f859961ed38
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-03-19 20:04:24 -0600

    Create Controladora.java
    Creacion de controladora
    Creación de métodos de la controladora

---
+++ Controladora.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+/**
+ * @author pc
+ *
+ */
+public class Controladora {
+	public void agregar(String categoria, String producto) {
+		
+	}
+	public String MostrarCategoria(String producto){
+		return "";
+	}
+	public String MostrarProductos(int categoria) {
+		return "";
+	}
+	public String MostrarProductosOrdenados(int categoria) {
+		return "";
+	}
+	public String MostrarInventario() {
+		return "";
+	}
+	public String MostrarInventarioOrdenado() {
+		return "";
+	}
+}
